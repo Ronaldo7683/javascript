51e8d3be50c2a72de5fe574f71c23145537f9f2b
Ronaldo Araújo <ronaldo7683@hotmail.com>
2023-08-09 22:22:16 -0300

    praticando

---
+++ meus_exercicios/exemplo_if_else.js
@@ -0,0 +1,14 @@
+let dia = true;
+if (dia) {
+    console.log("Luz Desligada");
+}
+else {
+    console.log("Luz Ligada");
+}
+
+let number = 98;
+if ( number === 98  ) {
+    console.log("É compativel");
+}else {
+    console.log("Não compativél");
+}
---
+++ meus_exercicios/quiz.js
@@ -0,0 +1,6 @@
+let fut = "bola";
+let canDrive = "errada";
+if (fut === "bola") {  // Esse bloco só executa se a resposta bola na variavel estiver correta.
+    canDrive = "correta";
+}
+console.log(fut + " Essa Resposta Esta " + canDrive);
